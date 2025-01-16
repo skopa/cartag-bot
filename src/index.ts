9eb7ad754a740e6af92290a6a520c7a6fb2b4e2e
Stepan Skopivskiy <skopik.stepan@gmail.com>
2025-01-16 12:27:15 +0200

    Added global error handler

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,33 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import { defineString } from 'firebase-functions/params';
 
 import { onCommand, onText, onPhoto } from './commands';
 import { initBot } from './bot';
 
 admin.initializeApp();
 
-
-/**
- * Firebase region
- */
-const region: string = defineString('FIREBASE_REGION').value();
-
-
 /**
  * Entry point
  */
-export const telegramBotWebhook = functions.region(region).https.onRequest(
+export const telegramBotWebhook = functions.region('europe-west1').https.onRequest(
     async (req, res) => {
-        if (req.get('content-type') === 'application/json') {
-            const bot = initBot();
-            console.log('Processing request: ', JSON.stringify(req.body));
+        try {
+            if (req.get('content-type') === 'application/json') {
+                const bot = initBot();
+                console.log('Processing request: ', JSON.stringify(req.body));
 
-            onCommand(bot);
-            onPhoto(bot);
-            onText(bot);
+                onCommand(bot);
+                onPhoto(bot);
+                onText(bot);
 
-            await bot.handleUpdate(req.body);
+                await bot.handleUpdate(req.body);
+                res.status(200).send('OK');
+            } else {
+                res.status(400).send('Invalid request');
+            }
+        } catch (e) {
+            console.error('Error', e);
             res.status(200).send('OK');
-        } else {
-            res.status(400).send('Invalid request');
         }
     },
 );
